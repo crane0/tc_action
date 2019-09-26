ebbfcd3758a2f4a51c02e4c1d2e364a72acc0248
crane0 <bhad5683126@163.com>
2019-09-26 11:10:31 +0800

    generics_class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-// import './enum'
-// import './interface_object'
-// import './interface_fun'
-// import './function'
-// import './class1'
-// import './class2'
-import './class_interface'
+// import './06enum'
+// import './07interface_object'
+// import './08interface_fun'
+// import './09function'
+// import './10class1'
+// import './11class2'
+// import './12class_interface'
+import './14generics_class'
 
 let hello : string = 'Hello ts'
 document.querySelectorAll('.app')[0].innerHTML = hello
